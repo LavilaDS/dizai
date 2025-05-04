609c8b7cd941419d341edd23b68736f4aa2d7a8a
Leandro Lávila Dias Souza <leandrolavila16@gmail.com>
2025-05-04 20:12:22 -0300

    feat: implement Router class for SPA navigation

---
+++ frontend/src/router.js
@@ -0,0 +1,40 @@
+// Router for SPA navigation
+class Router {
+    constructor(routes) {
+      this.routes = routes;
+      this.currentComponent = null;
+      
+      window.addEventListener('popstate', () => this.handleRoute());
+      this.handleRoute();
+      
+  
+      document.addEventListener('click', (e) => {
+        const link = e.target.closest('a');
+        if (link && link.href.startsWith(window.location.origin)) {
+          e.preventDefault();
+          this.navigate(link.href);
+        }
+      });
+    }
+  
+    async handleRoute() {
+      const path = window.location.pathname;
+      const route = this.routes[path] || this.routes['/'];
+  
+      if (this.currentComponent && this.currentComponent.unmount) {
+        this.currentComponent.unmount();
+      }
+  
+      const component = new route();
+      await component.render();
+      this.currentComponent = component;
+    }
+  
+    navigate(url) {
+      window.scrollTo(0, 0);
+      window.history.pushState({}, '', url);
+      this.handleRoute();
+    }
+}
+  
+export default Router;
