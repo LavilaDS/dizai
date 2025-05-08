5b5c7fe49d95e6c2f4b6185b6fb921c3e1892fb9
Leandro Lávila Dias Souza <leandrolavila16@gmail.com>
2025-05-08 21:54:12 +0000

    feat: add cookie-parser middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,14 +2,15 @@ const { env } = require('./config');
 const express = require('express');
 const routes = require('./routes');
 const errorHandler = require('./middleware/errorHandler');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api', routes);
 app.use(errorHandler);
 
 app.listen(env.port, () => {
   console.log(`Servidor rodando na porta ${env.port}`);
-}
-);
+});
