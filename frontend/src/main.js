a574ae74953279118b7c04cc130a8367839f6cb1
Leandro Lávila Dias Souza <leandrolavila16@gmail.com>
2025-05-08 13:03:39 +0000

    feat: add signup route to main router configuration

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,13 @@ import Router from "./router.js";
 import Home from "./pages/Home.js";
 import LearMore from "./pages/LearnMore.js";
 import Login from "./pages/Login.js";
+import Signup from "./pages/Signup.js";
 
 const routes = {
   "/": Home,
   "/about": LearMore,
   "/login": Login,
+  "/signup": Signup,
 };
 
 document.addEventListener("DOMContentLoaded", () => {
