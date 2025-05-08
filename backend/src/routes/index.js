3dd610edd74575c45adcb5c0d47eb74e4732322d
Leandro Lávila Dias Souza <leandrolavila16@gmail.com>
2025-05-08 21:54:40 +0000

    feat: add authRoutes to the main router

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const authRoutes = require('./authRoutes');
 const managerRoutes = require('./managerRoutes');
 
 router.use('/managers', managerRoutes);
+router.use('/auth', authRoutes);
 
 module.exports = router;
