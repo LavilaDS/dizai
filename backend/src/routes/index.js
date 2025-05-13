3f236f05eb57a9a66db7d9935e1c62861a604e1d
Leandro Lávila Dias Souza <leandrolavila16@gmail.com>
2025-05-13 19:51:37 +0000

    feat: add campaign routes with authentication middleware

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,9 +4,13 @@ const router = express.Router();
 const authRoutes = require('./authRoutes');
 const managerRoutes = require('./managerRoutes');
 const questionnairesRoutes = require('./questionnairesRoutes');
+const campaignRoutes = require('./campaignRoutes');
+const authMiddleware = require('../middleware/authMiddleware.js');
+
 
 router.use('/managers', managerRoutes);
 router.use('/auth', authRoutes);
 router.use('/questionnaires', questionnairesRoutes);
+router.use('/campaigns', authMiddleware, campaignRoutes);
 
 module.exports = router;
